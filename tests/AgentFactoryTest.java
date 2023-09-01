a46f1939d7f576ac3b20f53020c3646f6e12e9d0
Yuvalz19500 <yuvalz19500@gmail.com>
2023-09-01 13:06:36 +0300

    Fix test error

--- tests/AgentFactoryTest.java
+++ tests/AgentFactoryTest.java
@@ -1,28 +1,38 @@
 
 import Agent.*;
+import Audit.Audit;
 import BoSAgent.BoSAgent;
 import Mailer.Mailer;
 import ArgsSerializer.GameType;
 import PDAgent.PDAgent;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AgentFactoryTest {
+     Audit audit;
+     Mailer mailer;
+
+     @BeforeEach
+     void setUp() {
+         audit = new Audit();
+         mailer = new Mailer(audit);
+     }
 
     @Test
     void testCreatePDAgent() {
-        Agent agent = AgentFactory.createAgent(GameType.PD, 1,2, new Mailer(), null, 0);
+        Agent agent = AgentFactory.createAgent(GameType.PD, 1,2, mailer, null, 0);
         assertTrue(agent instanceof PDAgent, "Expected instance of PDAgent");
     }
 
     @Test
     void testCreateBoSAgent() {
-        Agent agent = AgentFactory.createAgent(GameType.BoS, 1,2, new Mailer(), null, 0);
+        Agent agent = AgentFactory.createAgent(GameType.BoS, 1,2, mailer, null, 0);
         assertTrue(agent instanceof BoSAgent, "Expected instance of BoSAgent");
     }
 
     @Test
     void testInvalidGameType() {
-        assertThrows(IllegalArgumentException.class, () -> AgentFactory.createAgent(null, 1,2, new Mailer(), null, 0), "Expected IllegalArgumentException for null GameType");
+        assertThrows(IllegalArgumentException.class, () -> AgentFactory.createAgent(null, 1,2, mailer, null, 0), "Expected IllegalArgumentException for null GameType");
     }
 }
