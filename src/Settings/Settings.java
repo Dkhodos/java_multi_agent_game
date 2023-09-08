4597361f6c283b8a27f4321bfcc8dd768d049a1d
dannyk <daniel@prodbase.io>
2023-09-08 12:46:01 +0300

    migrate to eclipse support

--- src/Settings/Settings.java
+++ src/Settings/Settings.java
@@ -3,7 +3,7 @@ package Settings;
 public class Settings {
 
     // enable / disable debug logs in program
-    public static boolean DEBUG = true;
+    public static boolean DEBUG = false;
 
     // agent sleep time between every message he reads (0 to disable sleep all together)
     public static int AGENT_SLEEP_TIME = 1;
